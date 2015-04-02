6a147196e448c5c8e0d79a3f3c13e57c684d68ee
Maximilian Timmerkamp <maximilian.timmerkamp@web.de>
2015-04-02 19:21:39 +0200

    Added documentation to MapNode and MapTerrainPhysicsControl.

--- src/caveyard/map/MapNode.java
+++ src/caveyard/map/MapNode.java
@@ -12,45 +12,85 @@ public class MapNode extends Node
 {
 	public static final String MAP_NODE_NAME = "mapNode";
 
+	/**
+	 * Map which owns this node.
+	 */
 	protected Map map;
 
-	protected MapControl mapControl;
+	/**
+	 * The map's LOD control.
+	 */
+	protected MapLODControl mapLODControl;
+	/**
+	 * The map's physics control which handles all terrain physics.
+	 */
 	protected MapTerrainPhysicsControl mapPhysics;
 
+	/**
+	 * Creates a new Node. {@link #mapLODControl} and {@link #mapPhysics} are set to null.
+	 * If a {@link MapLODControl} is attached to this node, this node's attribute
+	 * mapLODControl will be set automatically. The same happens when a
+	 * {@link MapTerrainPhysicsControl} is attached.
+	 *
+	 * @param map Map which owns this map node.
+	 */
 	public MapNode(Map map)
 	{
 		this(map, null, null);
 	}
 
-	public MapNode(Map map, MapControl mapControl, MapTerrainPhysicsControl mapPhysics)
+	public MapNode(Map map, MapLODControl mapLODControl, MapTerrainPhysicsControl mapPhysics)
 	{
 		super(MAP_NODE_NAME);
 
 		this.map = map;
-		this.mapControl = mapControl;
+		this.mapLODControl = mapLODControl;
 		this.mapPhysics = mapPhysics;
+
+		this.map.mapNode = this;
 	}
 
+	/**
+	 * Get the map which owns this node.
+	 * @return The owning map.
+	 */
 	public Map getMap()
 	{
 		return map;
 	}
 
-	public MapControl getMapControl()
+	/**
+	 * Get the LOD control.
+	 * @return The map's MapLODControl.
+	 */
+	public MapLODControl getMapLODControl()
 	{
-		return mapControl;
+		return mapLODControl;
 	}
 
-	public void setMapControl(MapControl mapControl)
+	/**
+	 * Set the map LOD control. A map only has one LOD control.
+	 * @param mapLODControl The map's LOD control.
+	 */
+	public void setMapLODControl(MapLODControl mapLODControl)
 	{
-		this.mapControl = mapControl;
+		this.mapLODControl = mapLODControl;
 	}
 
+	/**
+	 * Gets the map's  physics control of the terrain.
+	 * @return The current physics control.
+	 */
 	public MapTerrainPhysicsControl getMapPhysics()
 	{
 		return mapPhysics;
 	}
 
+	/**
+	 * Sets the map's physics control. A map only has one terrain physics
+	 * control.
+	 * @param mapPhysics The map's terrain physics control.
+	 */
 	public void setMapPhysics(MapTerrainPhysicsControl mapPhysics)
 	{
 		this.mapPhysics = mapPhysics;
--- src/caveyard/map/MapTerrainPhysicsControl.java
+++ src/caveyard/map/MapTerrainPhysicsControl.java
@@ -11,16 +11,39 @@ import java.util.Iterator;
 
 /**
  * A control which updates the physics space to contain all
- * necessary Collision shapes of the terrain.
+ * necessary Collision shapes of the map's terrain.
+ *
+ * This control can be added to {@link MapNode}s only. Otherwise an
+ * exception will be thrown.
+ *
+ * @see #setSpatial(Spatial)
  *
  * @author Maximilian Timmerkamp
  */
 public class MapTerrainPhysicsControl extends AbstractControl
 {
+	/**
+	 * The physical environment of the map.
+	 */
 	protected PhysicsSpace physicsSpace;
+	/**
+	 * The map node this control is attached to. Also stored in
+	 * {@link #spatial} but in a less strong data type.
+	 */
+	protected MapNode mapNode;
+	/**
+	 * The map of the current map node for easy access.
+	 */
 	protected Map map;
 
+	/**
+	 * Cells in the physics space since last update.
+	 */
 	protected HashSet<Cell> oldVisibleCells;
+	/**
+	 * Determines if an update of the physics space is performed on
+	 * the next {@link #controlUpdate(float)} call.
+	 */
 	protected boolean needsUpdate;
 
 	public MapTerrainPhysicsControl(PhysicsSpace physicsSpace)
@@ -29,26 +52,50 @@ public class MapTerrainPhysicsControl extends AbstractControl
 
 		this.oldVisibleCells = new HashSet<>();
 		this.needsUpdate = true;
+
+		mapNode = null;
+		map = null;
 	}
 
+	/**
+	 * Determines if this control updates during the next update call.
+	 * @return Value of the update flag.
+	 */
 	public boolean isNeedsUpdate()
 	{
 		return needsUpdate;
 	}
 
+	/**
+	 * Sets a flag which determines if the next call to {@link #controlUpdate(float)}
+	 * updates the terrain collision shapes in the physics space.
+	 * This method is used by {@link MapLODControl} to tell this control
+	 * about changes in the displayed terrain cells.
+	 *
+	 * @param needsUpdate If update is needed on next update call.
+	 */
 	public void setNeedsUpdate(boolean needsUpdate)
 	{
 		this.needsUpdate = needsUpdate;
 	}
 
+	/**
+	 * This control can only be added to {@link MapNode}s otherwise
+	 * an exception is thrown. It does not make any sense to add this control
+	 * to any other sort of node as only maps can be updated by it.
+	 *
+	 * @param spatial Map node to use.
+	 */
 	@Override
 	public void setSpatial(Spatial spatial)
 	{
 		if (spatial instanceof MapNode)
 		{
 			super.setSpatial(spatial);
-			this.map = ((MapNode) spatial).getMap();
-			((MapNode) spatial).setMapPhysics(this);
+			this.mapNode = (MapNode) spatial;
+			mapNode.setMapPhysics(this);
+
+			this.map = mapNode.getMap();
 		}
 		else
 		{
@@ -57,10 +104,14 @@ public class MapTerrainPhysicsControl extends AbstractControl
 		}
 	}
 
+	/**
+	 * Updates the collision information of the physics space to ensure
+	 * correct collision with other objects.
+	 */
 	@Override
 	protected void controlUpdate(float tpf)
 	{
-		if (!needsUpdate) return;
+		if (!needsUpdate || mapNode == null) return;
 
 		Iterator<Cell> it = oldVisibleCells.iterator();
 		while(it.hasNext())
